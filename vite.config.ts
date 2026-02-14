c28656bcb0fc007ef65c18f46fc40cbb04cfe72b
CHIMI <stephendauda001@gmail.com>
2026-02-14 11:38:05 -0800

    fix base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/mywebsite/',  
-  plugins: [react()],
+  base: "/my-website/",
+  plugins: [react()]
 })
